150911b2882073163961fa93d56f23c170621699
plumchloride <70005022+plumchloride@users.noreply.github.com>
2022-10-27 09:09:36 +0900

    add change color

--- canvas.js
+++ canvas.js
@@ -7,17 +7,17 @@ let font_size = 48
 // let font = "serif"
 let font = "'ＭＳ ゴシック'"
 let lineheight = 30
-let text_start = 150
+let text_start = 100
 
 
 var timeoutID = 0;
 var delay = 300;
 window.addEventListener("resize",()=>{
   if(now_read){
-    run = false;
+    nove_stop();
     clearTimeout(timeoutID);
     timeoutID = setTimeout(function(){
-        create_hayayomi(save.bun,save.index,save.talk,false);
+        create_hayayomi(save.bun,save.index,save.talk,save.name,false);
     }, delay);
   }
 })
@@ -25,55 +25,113 @@ window.addEventListener("resize",()=>{
 
 
 let run = false;
-let save = {"index":1,"bun":["前の文","今の文","後の文"],"talk":[false,false,false]}
-const create_hayayomi = (arr,index,talk_arr,continue_flag = true)=>{
+let save = {"index":0,"bun":[],"talk":[],"name":"アプリ説明"}
+save.bun = ['hayayomiとは', '', '高速逐次', '視覚提示', '', '（RSVP）', 'を用いて', '高速に', '読書を', '行う事が', '出来る', 'アプリケーション', 'です。', 'RSVPとは', '同じ場所に', '単語を', '高速で表示する','（フラッシュ', '暗算のように）', '事で読書に', '必要な','眼球運動を無くし', '高速に', '文字を', '読むことが', '出来る手法です。', 'ただし','この手法では', '文字を', '後戻りする事が','出来ず、', '文章への', '理解力に', '影響が出ます。', 'また、', '適切な速度で','読書を', '行う事で', 'ストレスレベル', '低下に', 'つながるため、', '本', 'アプリケーション', 'では', 'リラックス', '目的には', '向いて', 'いません。', '以上より、', '内容への', '適切な理解や', 'ストレスレベル', '低下には', '役立つ事は', 'ありませんが、', '文章の', '流し読みや', '全体の流れ', 'の把握に', '用いる事が', '出来ます。', '高速逐次', '視覚提示に', '用いる文章の', '分かち書きは', 'すべて', 'ユーザーサイドで', '行って', 'おり、', '通信を', '行わない', 'ため', 'セキュリティ面','でも安全に', 'ご利用','いただけます。']
+save.talk = [false, false, true, true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false]
+const $progress = {"cu":document.getElementById("index_cu"),"max":document.getElementById("index_max"),"bar":document.getElementById("novel_volume")}
+let read_timeout = 0;
+const create_hayayomi = (arr,index,talk_arr,save_name,continue_flag = true)=>{
+  index = Number(index)
   $canvas.width = $canvas.clientWidth
   $canvas.height = $canvas.clientHeight
   max_canvas_size = {x:$canvas.clientWidth,y:$canvas.clientHeight}
 
+  // BG作製
+  ctx.fillStyle = current_col.bg;
+  ctx.beginPath();
+  ctx.rect(0,0,max_canvas_size.x,max_canvas_size.y);
+  ctx.fill();
   // 本体生成
   if(talk_arr[index]){
-    ctx.fillStyle = 'red';
+    ctx.fillStyle = current_col.sub;
   }else{
-    ctx.fillStyle = 'green';
+    ctx.fillStyle = current_col.prime;
   }
   ctx.textAlign = "center";
   ctx.textBaseline = "top";
+
   ctx.font = "bold " + font_size + 'px '+ font;
   // console.log(max_canvas_size);
   ctx.fillText(arr[index],max_canvas_size.x/2,text_start+(font_size/2 + lineheight))
+
   if(talk_arr[index-1]){
-    ctx.fillStyle = 'red';
+    ctx.fillStyle = current_col.sub;
   }else{
-    ctx.fillStyle = 'green';
+    ctx.fillStyle = current_col.prime;
   }
   ctx.font = font_size/2 + 'px '+ font;
   ctx.fillText(arr[index-1]||"",max_canvas_size.x/2,text_start)
+
   if(talk_arr[index+1]){
-    ctx.fillStyle = 'red';
+    ctx.fillStyle = current_col.sub;
   }else{
-    ctx.fillStyle = 'green';
+    ctx.fillStyle = current_col.prime;
   }
   ctx.fillText(arr[index+1]||"",max_canvas_size.x/2,text_start+(font_size/2 + font_size + lineheight * 2))
 
-  // 進捗表示
-  ctx.beginPath();
-  ctx.rect(max_canvas_size.x/10,50,8*(max_canvas_size.x/10),50);
-  ctx.stroke();
+  // // 進捗表示
+  // ctx.beginPath();
+  // ctx.rect(max_canvas_size.x/10,50,8*(max_canvas_size.x/10),50);
+  // ctx.stroke();
+
+  // 進捗変更
+  $progress.cu.innerText = index
+  $progress.max.innerText = arr.length-1
+  $progress.bar.value = index
+  $progress.bar.max = arr.length-1
+  // 名前表示変更
+  if(save_name != save.name){
+    document.getElementById("read_title").innerText = save_name
+  }
+
+  save = {"index":index,"bun":arr,"talk":talk_arr,"name":save_name}
   if(continue_flag && run){
   // if(true){
     if(index >  arr.length-2){
       ;
     }else{
-      setTimeout(()=>{create_hayayomi(arr,index+=1,talk_arr)},100)
+      var delay_time = 0
+      var c_min_del = Number(min_del)
+      arr[index].split("").forEach(e => {
+        if(kanahiraeng_cha.indexOf(e)!=-1){
+          delay_time +=kana_del
+        }else if(mini_cha.indexOf(e)!=-1){
+          delay_time +=mini
+        }else if(fin_cha.indexOf(e)!=-1){
+          c_min_del +=fin
+          delay_time +=fin
+        }else{
+          delay_time +=kanzi_del
+        }
+      });
+      delay_time = delay_time>=min_del ? delay_time:min_del;
+      clearTimeout(read_timeout);
+      read_timeout = setTimeout(()=>{create_hayayomi(arr,index+=1,talk_arr,save_name)},delay_time)
     }
   }else{
-    save = {"index":index,"bun":arr,"talk":talk_arr}
+    nove_stop();
   }
 }
-let kana_del = 100;
+let kana_del = 50;
 let mini = 50;
-let kanzi_del = 200;
-let min_del = 300;
-const kanahiraeng_cha = `あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわをんがぎぐげござじずぜぞだぢづでどばびぶべぼぱぴぷぺぽっアイウエオカキクケコサシスセソタチツテトナニヌネノハヒフヘホマミムメモヤユヨラリルレロワヲンガギグゲゴザジズゼゾダヂヅデドバビブベボパピプペポッqwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM、。!?！？`
+let fin = 100;
+let kanzi_del = 70;
+let min_del = 200;
+const kanahiraeng_cha = `あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわをんがぎぐげござじずぜぞだぢづでどばびぶべぼぱぴぷぺぽっアイウエオカキクケコサシスセソタチツテトナニヌネノハヒフヘホマミムメモヤユヨラリルレロワヲンガギグゲゴザジズゼゾダヂヅデドバビブベボパピプペポッqwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM`
 const mini_cha = `ゃゅょぁぃぅぇぉャュョァィゥェォ`
+const fin_cha = `、。!?！？`
+
+const $cont = {"start":document.getElementById("play"),"stop":document.getElementById("pause")}
+const nove_stop = ()=>{
+  clearTimeout(read_timeout);
+  run = false;
+  $cont.start.classList.remove("non-visi")
+  $cont.stop.classList.add("non-visi")
+}
+const nove_start = ()=>{
+  clearTimeout(read_timeout);
+  run = true;
+  $cont.start.classList.add("non-visi")
+  $cont.stop.classList.remove("non-visi")
+  create_hayayomi(save.bun,save.index,save.talk,save.name,true);
+}
--- ui.js
+++ ui.js
@@ -2,8 +2,9 @@ const nav_list = ['rea','hon','ser','inf'];
 const nav = {'rea':document.getElementById('nav_rea'),'hon':document.getElementById('nav_hon'),'ser':document.getElementById('nav_ser'),'inf':document.getElementById('nav_inf')};
 const cont = {'rea':document.getElementById('rea'),'hon':document.getElementById('hon'),'ser':document.getElementById('ser'),'inf':document.getElementById('inf')};
 let now_read = false
-const changepage = (topage)=>{
+const changepage = (topage,rerode = true)=>{
   var read_change = false
+  nove_stop();
   nav_list.forEach(e=>{
     if(e == topage){
       nav[e].classList .add("nav-current");
@@ -15,8 +16,7 @@ const changepage = (topage)=>{
     if(topage == "rea" && e == topage){
       now_read = true
       read_change = true
-      run = false
-      create_hayayomi(save.bun,save.index,save.talk,false);
+      if(rerode)create_hayayomi(save.bun,save.index,save.talk,save.name,false);
     }else if(!read_change){
       now_read = false
     }
@@ -27,8 +27,65 @@ const changepage = (topage)=>{
 // 今すぐ読むクリック
 let segmented_dic = {}
 async function add_read(index){
+  nove_stop();
   text = await get_aozora(serch_data.text_url[index],serch_data.decord[index])
   segmented_dic = await segment(text)
-  changepage("rea")
-  create_hayayomi(segmented_dic.text,0,segmented_dic.talk,false)
+  changepage("rea",false)
+  create_hayayomi(segmented_dic.text,0,segmented_dic.talk,serch_data.name[index],false)
+}
+// 本棚に追加をクリック
+const add_bookshelf = (index)=>{
+  alert("本棚機能は未実装です。今後実装する予定です。")
+}
+
+
+document.getElementById("novel_volume").addEventListener("input",(e)=>{
+  nove_stop();
+  $progress.cu.innerText = e.target.value
+  create_hayayomi(save.bun,e.target.value,save.talk,save.name,false);
+})
+document.getElementById("sk_prev").addEventListener("click",(e)=>{
+  nove_stop();
+  $progress.cu.innerText = e.target.value
+  create_hayayomi(save.bun,save.index -1,save.talk,save.name,false);
+})
+document.getElementById("sk_next").addEventListener("click",(e)=>{
+  nove_stop();
+  $progress.cu.innerText = e.target.value
+  create_hayayomi(save.bun,save.index +1,save.talk,save.name,false);
+})
+
+
+
+// イニシャライズ
+// 色追加
+let col = {};
+let current_col = {"bg": '#fffaf0', "prime": '#008000', "sub": '#ff0000'}
+const $col_picer = document.getElementById("backgrounds");
+(()=>{
+  var _request = new XMLHttpRequest();
+  _request.addEventListener('load', (event) => {
+    var response = event.target.responseText;
+    var row = response.split(/\r\n|\n/);
+    row.forEach((e,index)=>{
+      var data = e.split(",")
+      col[index] = {"bg":data[0],"prime":data[1],"sub":data[2]}
+      var c_class = index == 0?' bg_ex-current"':""
+      $col_picer.innerHTML += `<div class="bg_ex${c_class}" onclick="bg_change(${index})" id="bg-${index}" style="background-color:${data[0]};"><span style="color:${data[1]};">地の文</span><br><span style="color:${data[2]};">会話文</span></div>`
+    })
+  });
+  _request.open('GET', "./color.csv", true);
+  _request.send();
+})();
+
+const bg_change = (index)=>{
+  Object.keys(col).forEach(e=>{
+    document.getElementById(`bg-${e}`).classList.remove("bg_ex-current")
+  })
+  document.getElementById(`bg-${index}`).classList.add("bg_ex-current")
+  current_col = col[index]
+  document.documentElement.style.setProperty('--bg',current_col.bg);
+  document.documentElement.style.setProperty('--text',current_col.prime);
+  document.documentElement.style.setProperty('--speak',current_col.sub);
+  create_hayayomi(save.bun,save.index,save.talk,save.name,false);
 }
